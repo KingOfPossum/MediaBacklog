d8eb3a160bb8c416c102bd01e07d55d92ffd3344
Paul Mann <paulmann2205@gmail.com>
2026-06-12 15:48:10 +0200

    Header and Layout

---
+++ frontend/app/layouts/test.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import MainSidebar from "~/components/MainSidebar.vue";
+</script>
+
+<template>
+  <div class="h-screen flex overflow-hidden">
+    <div class="flex flex-1">
+      <MainSidebar />
+
+      <div class="w-full">
+        <UHeader title="">
+          <template #left>
+            <span>Filter</span>
+            <UButton icon="i-lucide:filter" color="neutral" variant="outline" size="xl"/>
+          </template>
+
+          <template #default>
+            <UInput class="w-30vw" size="xl" placeholder="Search..." color="neutral" variant="outline" icon="i-lucide-search"/>
+          </template>
+
+          <template #right>
+            <UButton icon="i-lucide:list" color="neutral" variant="outline" size="xl" />
+          </template>
+        </UHeader>
+
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,23 +1,9 @@
 <script setup lang="ts">
-import GameView from "~/components/GameView.vue";
-import MainSidebar from "~/components/MainSidebar.vue";
+definePageMeta({
+  layout:'test'
+})
 </script>
 
 <template>
-  <div class="h-screen flex overflow-hidden">
-    <div class="flex flex-1">
-      <MainSidebar />
-
-      <div>
-        <div class="mt-5 ml-20 mr-20 flex">
-          <span class="pt-1 mr-3">Filter</span>
-          <UButton icon="i-lucide:filter" color="neutral" variant="outline" size="xl"/>
-          <UInput class="w-[50%] ml-[20%]" placeholder="Search..." color="neutral" variant="outline" icon="i-lucide-search"/>
-          <UButton class="ml-[20%]" icon="i-lucide:list" color="neutral" variant="outline" size="xl" />
-        </div>
-
-        <GameView />
-      </div>
-    </div>
-  </div>
+  <GameView />
 </template>
